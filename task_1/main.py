5210563ecaaa0aef845be6170c669e18d314fa59
Dark L1ght <georgmaximov160805@gmail.com>
2025-03-05 19:12:46 +0700

    lab2 doing

--- task_1/main.py
+++ task_1/main.py
@@ -5,22 +5,21 @@ import csv
 
 def main(): 
     hostnames = ["google.com", "cloudflare.com", "stackoverflow.com", "github.com", "discord.com", 
-                 "steampowered.com", "aeza.net", "yandex.com", "yahoo.com", "chess.com"]
-    timeout_time = 0.1
-    rtt_arr = []
-    r_exp = r'\d+'
+        "steampowered.com", "aeza.net", "yandex.com", "yahoo.com", "chess.com"]
+    rtt_array = []
+    regular_expression = r'\d+'
 
     for hostname in hostnames:
         response = str(subprocess.run(f"ping -n 1 {hostname}", 
-                                      capture_output=True, shell=True, text=True, encoding='cp866').stdout)
-        regexp_output = re.findall(r_exp, response)
+            capture_output=True, shell=True, text=True, encoding='cp866').stdout)
+        regexp_output = re.findall(regular_expression, response)
         if len(regexp_output) < 1:
             regexp_output = [-1]
-        rtt_arr.append(regexp_output[-1])
+        rtt_array.append(regexp_output[-1])
 
-    csv_to_write = zip(hostnames, rtt_arr)
+    csv_to_write = zip(hostnames, rtt_array)
 
-    with open('rtts.csv', 'w') as csvfile:
+    with open('task_1/rtts.csv', 'w') as csvfile:
         writer = csv.writer(csvfile, dialect='excel')
         for hostname, rtt in csv_to_write:
             writer.writerow([hostname, rtt])
--- task_2/main.py
+++ task_2/main.py
@@ -11,16 +11,37 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 
-chrome_driver_path = "C:\\chromedriver-win64\\chromedriver.exe"
+# chrome_driver_path = "C:\\chromedriver-win64\\chromedriver.exe"
 
 option = Options()
 option.add_argument('--ignore-certificate-errors')
 option.add_argument('--ignore-ssl-errors')
 
-service = Service(chrome_driver_path)
+# service = Service(chrome_driver_path)
 
-browser = webdriver.Chrome(service=service, options=option)
-browser.get('https://www.drom.ru/')
+browser = webdriver.Chrome(options=option)
+browser.get('https://auto.drom.ru/')
 
-sign_in_buttn = browser.find_element(By.CSS_SELECTOR, "_1tt78qz3.oco7hz0")
-sign_in_buttn.click()
+# sign_in_buttn = WebDriverWait(browser, 10).until(
+#     EC.presence_of_element_located((By.CSS_SELECTOR, "._1tt78qz3.oco7hz0")))
+# sign_in_buttn.click()
+# print("Done!")
+
+# login_element = browser.find_element(By.NAME, "sign")
+# login_element.send_keys('89134808098' + Keys.RETURN)
+
+# password_element = browser.find_element(By.NAME, "password")
+# password_element.send_keys('GeorgMax121270' + Keys.RETURN)
+
+# time.sleep(10)
+
+# sign_button = browser.find_element(By.ID, "signbutton")
+# sign_button.click()
+
+# time.sleep(10)
+
+
+vehicles = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "css-1f68fiz.ea1vuk60")))
+print(vehicles)
+time.sleep(5)
