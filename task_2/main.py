ef91ed35696a05ffcfb460f4858b7fa58c8fb58b
Dark L1ght <georgmaximov160805@gmail.com>
2025-03-06 17:31:51 +0700

    lab2_bebebe

--- task_2/main.py
+++ task_2/main.py
@@ -1,5 +1,4 @@
 import csv
-import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,41 +6,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-'''
-# authorization code:
-sign_in_buttn = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "._1tt78qz3.oco7hz0")))
-sign_in_buttn.click()
-print("Done!")
-
-login_element = browser.find_element(By.ID, "sign")
-login_element.send_keys('' + Keys.RETURN)
-
-password_element = browser.find_element(By.NAME, "password")
-password_element.send_keys('' + Keys.RETURN)
-
-time.sleep(10)
-
-sign_button = browser.find_element(By.ID, "signbutton")
-sign_button.click()
-
-time.sleep(10)'''
+option = webdriver.ChromeOptions()
+browser = webdriver.Chrome(options=option)
 
 
-def main():
-    pages = 5
-    vehicles_data = []
-
-    option = webdriver.ChromeOptions()
-    option.add_argument('--ignore-certificate-errors')
-    option.add_argument('--ignore-ssl-errors')
-
-    browser = webdriver.Chrome(options=option)
+def load_page():
     browser.get('https://auto.drom.ru/')
 
     km_buttn = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.XPATH, "//a[contains(@href, 'drom.ru/auto/?distance=100')]")))
     km_buttn.click()
+
+
+def parse_pages(pages):
+    vehicles_data = []
 
     for page in range(1, pages + 1):
         browser.get(f'https://novosibirsk.drom.ru/auto/page{page}/?distance=100')
@@ -58,12 +36,27 @@ def main():
 
             vehicles_data.append([title, description, price, location, date])
 
-    with open('parsed_data.csv', 'w', encoding='utf-8', newline='') as csv_file:
+    return vehicles_data
+
+
+def save_to_csv(filename, vehicles_data):
+    with open(filename, 'w', encoding='utf-8', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, dialect='excel')
         csv_writer.writerows(vehicles_data)
 
     print('Saved to csv file')
 
 
-main()
+def main():
+    num_pages = 5
+    filename = 'parsed_data.csv'
 
+    option.add_argument('--ignore-certificate-errors')
+    option.add_argument('--ignore-ssl-errors')
+
+    load_page()
+    vehicles_data = parse_pages(num_pages)
+    save_to_csv(filename, vehicles_data)
+
+
+main()
